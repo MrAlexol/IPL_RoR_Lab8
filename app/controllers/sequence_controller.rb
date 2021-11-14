801fba02ec0ba40acb06e7a648c3e300b8d90447
MrAlexol <Alex02072002@yandex.ru>
2021-11-14 21:33:06 +0300

    v. 1. Everything works. No tests

---
+++ app/controllers/sequence_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Контроллер для последовательности
+class SequenceController < ApplicationController
+  def input; end
+
+  def view
+    begin
+      @input = params[:values]&.split&.map { |el| Integer el }
+      @error = 'Последовательность короче 10 чисел' if @input.length < 10
+      find_increasing_subseq
+    rescue ArgumentError
+      @error = 'Некорректный ввод'
+    rescue NoMethodError
+      @error = 'Последовательность не задана'
+    end
+  end
+
+  private
+
+  def find_increasing_subseq
+    @result = @input.each_with_object([[]]) do |el, acc|
+      acc << [] if el <=> acc[-1][-1] && el < acc[-1][-1]
+      acc[-1] << el
+    end
+  end
+end
